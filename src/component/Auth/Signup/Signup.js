dc2540822badec7abaa7ff28c0b50d0474841db6
ahmed-nasir <nasirahmed595@gmail.com>
2022-04-17 12:33:44 +0600

    add email handler

--- src/component/Auth/Signup/Signup.js
+++ src/component/Auth/Signup/Signup.js
@@ -3,9 +3,33 @@ import { Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Signup = () => {
-    const [useInfo,setUserInfo]=useState({
-
+    const [userInfo, setUserInfo] = useState({
+        email: "",
+        password: "",
+        confirmPassword: "",
     })
+
+    const [errors, setErrors] = useState({
+        emailError: "",
+        PasswordError: "",
+        general: "",
+    })
+
+    const handleEmailChange = (e) => {
+
+        const emailRegEx = /\S+@\S+\.\S+/;
+        const validEmail = emailRegEx.test(e.target.value)
+
+        if (validEmail) {
+            setUserInfo({ ...userInfo, email: e.target.value })
+            setErrors({ ...errors, emailError: "" })
+
+        } else {
+            setErrors({ ...errors, emailError: "Invalid Email" })
+            setUserInfo({ ...userInfo, email:"" })
+        }
+    }
+
     return (
         <div className='mt-5 border border-1 rounded-3 w-50 mx-auto'>
             <div className='text-center'>
@@ -13,20 +37,20 @@ const Signup = () => {
             </div>
             <div className='container p-3'>
                 <div className='d-flex justify-content-center '>
-                    <Form >
+                    <Form>
                         <Form.Group className="mb-3" controlId="formGroupEmail">
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control  type="email" placeholder="Enter email" />
-                            
+                            <Form.Control onChange={handleEmailChange} type="email" placeholder="Enter email" />
+                            {errors?.emailError && <p className='text-danger'>{errors.emailError}</p>}
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formGroupPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control  type="password" placeholder="Password" />
+                            <Form.Control type="password" placeholder="Password" />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formGroupConfirmPassword">
                             <Form.Label>Confirm Password</Form.Label>
-                            <Form.Control  type="password" placeholder="Confirm Password" />
-                            
+                            <Form.Control type="password" placeholder="Confirm Password" />
+
                         </Form.Group>
                         <Form.Group className='mb-3' controlId='toggle'>
                             <Form.Text className="text-muted">
